e585de181d84ba1389566776f281ada3eae891c7
Gurupada Chakraborty <gurupada.chakraborty@ge.com>
2022-08-11 07:11:04 +0530

    added a oute for file upload and test mongo connectivity

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,34 @@
 from application import app
-from flask import render_template
+from flask import render_template, request, flash, redirect, url_for
+import csv
+import pandas as pd
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/csvSave',methods = ['POST'])
+def upload_route_summary():
+    error = None
+    if request.method == 'POST':
+
+        # Create variable for uploaded file
+        try :
+            f = request.files['fileupload']  
+
+            #store the file contents as a string
+            
+            # fstring = f.read()
+            
+            df = pd.read_csv(f)
+            
+            #create list of dictionaries keyed by header row
+            # csv_dicts = [{k: v for k, v in row.items()} for row in csv.DictReader(fstring.splitlines(), skipinitialspace=True)]
+            
+            csv_dicts = df.head().to_dict('records')
+
+            #do something list of dictionaries
+            flash(u"Success !!", "success")
+        except Exception as e:
+            flash(u"Error : " +str(e), "danger" )
+    return redirect(url_for("index"))

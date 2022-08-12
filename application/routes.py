8c14a7059afed4b107ad66742a01640fb257c459
Gurupada Chakraborty <gurupada.chakraborty@ge.com>
2022-08-12 07:45:29 +0530

    finalizing changes and fine tuning with UI table cleanup

--- application/routes.py
+++ application/routes.py
@@ -1,51 +1,61 @@
 from application import app
 from flask import render_template, request, flash, redirect, url_for, jsonify
-import csv
 import pandas as pd
 import pymongo
 from . import stores
 
+# connecting to mongodb
 client = pymongo.MongoClient(app.config['MONGO_URI'])
 
 db = client['db']
 col = db['stores']
 
-
+# default home path
 @app.route('/')
 def index():
     return render_template("index.html")
 
+# route for saving records from csv file
 @app.route('/csvSave',methods = ['POST'])
 def upload_route_summary():
-    error = None
     if request.method == 'POST':
 
         # Create variable for uploaded file
         try :
+            # getting uploaded file content
             f = request.files['fileupload']  
-
-            #store the file contents as a string
-            
-            # fstring = f.read()
-            
             df = pd.read_csv(f)
-            
-            #create list of dictionaries keyed by header row
-            # csv_dicts = [{k: v for k, v in row.items()} for row in csv.DictReader(fstring.splitlines(), skipinitialspace=True)]
-            
             csv_dicts = df.to_dict('records')
-            
-            # col.insert_many(csv_dicts)
+            list_flag = []
             for i in csv_dicts:
                 resp = stores.Stores().set_store_details(i)
-                stores.Stores().add_record(resp)
-            #do something list of dictionaries
-            flash(u"Success !!", "success")
+                list_flag.append(stores.Stores().add_record(resp))
+            # defining messages to be sent through flash to ui
+            if list_flag.count(1) == len(list_flag):
+                flash(u"Success : Records added !", "success")
+            else:
+                flash(u"Validation error : {} out of {} records updated !".format(list_flag.count(1), len(list_flag)), "warning")
         except Exception as e:
             flash(u"Error : " +str(e), "danger" )
     return redirect(url_for("index"))
 
-@app.route("/listall", methods=['GET'])
-def fetchall():
-    cur = list(col.find({},{'_id':0}))
-    return jsonify({'data' : cur})
+# route for querying the db
+@app.route("/query", methods=['POST'])
+def query():
+    # getting json from UI and processing
+    data = request.json
+    # data = dict(data)
+    qp = {}
+    query_dict = {}
+    qp[data['fields']] = data['search']
+    if "Price" in [k for k,v in qp.items()]:
+        qp["Price"] = int(qp["Price"])
+        query_dict['price'] = qp["Price"]
+    if "Store ID" in [k for k,v in qp.items()]:
+        query_dict['store_id'] = qp['Store ID']
+    if "SKU" in [k for k,v in qp.items()]:
+        query_dict['sku'] = qp['SKU']
+    if "Product Name" in [k for k,v in qp.items()]:
+        query_dict['product_name'] = qp['Product Name']
+    res = list(col.find(query_dict, {"_id":0}))
+    return jsonify({'data' : res})
--- application/stores.py
+++ application/stores.py
@@ -1,9 +1,8 @@
-# from application import app
-from enum import unique
 import logging
 import datetime as datetime
 import mongoengine
 
+# connecting to mongodb
 mongoengine.connect(
     db='db',
     username='db_mongo_user',
@@ -13,16 +12,18 @@ mongoengine.connect(
 )
 logger = logging.getLogger(__name__)
 
+# schema definition
 class Stores(mongoengine.DynamicDocument):
-    store_id = mongoengine.IntField(required=True, unique=True)
+    store_id = mongoengine.StringField(required=True)
     sku = mongoengine.StringField(required=True, unique=True)
     product_name = mongoengine.StringField(required=True)
     price = mongoengine.IntField(required=True)
     date = mongoengine.DateTimeField(null=True)
 
+    # setting up schema object
     def set_store_details(self, record):
         store_schema = Stores()
-        store_schema.store_id = record['id']
+        store_schema.store_id = str(record['id'])
         store_schema.sku = record['sku']
         store_schema.product_name = record['name']
         store_schema.price = record['price']
@@ -33,10 +34,16 @@ class Stores(mongoengine.DynamicDocument):
 
         return store_schema
 
-    def add_record(self, store_schema):
+    # validating and adding records to db
+    @staticmethod
+    def add_record(store_schema):
+        flag = 0
         try :
             store_schema.validate()
             store_schema.save()
             print("Record saved successfully!")
+            flag = 1
+            return flag
         except Exception as e:
-            logger.error(e)
+            logger.error(str(e))
+            return flag
